9e3d66338ecf181d6a2714a600cfe46983f0f213
Matias Zabala <zabala@live.com.ar>
2024-10-01 18:06:34 -0300

    Agregado de eventos e incorporacion de planes a recomendar

--- script.js
+++ script.js
@@ -5,59 +5,109 @@ saludo.innerHTML = "<h1>Asistente dietario Gemini</h1>"
 
 let formulario = document.getElementById("formularioUsuario");
 
-formulario.innerHTML =
+formulario.addEventListener('submit', (e) => {
 
-    ` <ul>
-      <li>
-          <label for="Ingrese su nombre y apellido">Nombre y apellido</label>
-          <input type="text" id="nombre">
-      </li>
-      <li>
-          <label for="Ingrese su fecha de nacimiento">Fecha de nacimiento</label>                
-          <input type="date" id="fdn">
-                </li>
-                <li>
-                    <label for="Ingrese su peso en kilogramos">Peso actual</label>
-                    <input type="number" id="peso">
-                </li>
-                <li>
-                    <label for="Ingrese su altura en centimetros">Altura</label>
-                    <input type="number" id="altura">
-                </li>
-                <li>
-                    <label for="Genero">Genero</label>
-                    <select name="genero" id="genero">
-                        <option value="hombre">Masculino</option>
-                        <option value="mujer">Femenino</option>
-                    </select>
-                </li>
-                <li>
-                    <textarea name="Objetivo a alcanzar con la dieta" id="objetivo" placeholder="Objetivos del plan"></textarea>
-                </li>
-                <li><input type="submit"></li>
-    </ul>`;
+    e.preventDefault()
+    onclick = guardarDatos(); planObtenido();
 
-class Usuario {
+})
 
-    constructor(nombre, fdn, altura, peso, genero) {
+const plans = [
 
-        this.nombre = nombre;
-        this.altura = altura;
-        this.fdn = fdn;
-        this.peso = peso;
-        this.genero = genero;
+    { id: 1, nombre: 'Plan Alpha', precio: '23000' },
+    { id: 2, nombre: 'Plan Beta', precio: '25000' },
+    { id: 3, nombre: 'Plan Gamma', precio: '30000' },
+    { id: 4, nombre: 'Plan Epsilon', precio: '34000' },
+
+]
+
+function mostrarPlanes() {
+
+    plans.forEach(plan => {
+
+        let planes = document.getElementById("")
+
+        
+    });
+
+
+}
+
+const vael0 = 150
+const vael1 = 0.75
+const vael2 = 0.6
+const vael3 = 50
+
+let nombreUsuario = document.getElementById("nombre").value
+let fdnUsuario = document.getElementById("fdn").value
+let pesoUsuario = document.getElementById("peso").value
+let alturaUsuario = document.getElementById("altura").value
+let generoUsuario = document.getElementById("genero").value
+let objetivoUsuario = document.getElementById("objetivo").value
+
+const datos = [nombreUsuario, alturaUsuario, fdnUsuario, pesoUsuario, generoUsuario, objetivoUsuario]
+
+function planObtenido() {
+
+    if (objetivoUsuario == 'ganarMasa') {
+        let planRecomendado = document.getElementById('plan1')
+        planRecomendado.innerHTML = `<h4>Para ganar masa muscular te recomendamos el ${(plans[0].nombre)}, el cual incluye una completa rutina de ejercicios, ademas de un plan de alimentacion y asistencia on-line personalizada de nuestros coachs</h4>`
     }
+
+    if (objetivoUsuario == 'subirPeso') {
+        let planRecomendado = document.getElementById('plan1')
+        planRecomendado.innerHTML = `<h4>Si necesitas subir de peso te recomendamos el ${(plans[1].nombre)} el cual incluye un estricto plan dietario. Ademas contaras con la asistencia de profesionales cuando necesites</h4>`
+    }
+
+    if (objetivoUsuario == 'bajarPeso') {
+        let planRecomendado = document.getElementById('plan1')
+        planRecomendado.innerHTML = `<h4>Si necesitas bajar de peso te recomendamos el ${(plans[2].nombre)}, el cual incluye una rutina de ejercicios personalizada. Ademas contaras con un plan dietario y asistencia profesional cuando necesites</h4>`
+    }
+
 }
 
-const datos = [nombre, altura, fdn, peso, genero]
-function pesoIdeal1(altura, vael0, vael1, vael3) {
+function guardarDatos() {
 
-    return ((altura - vael0) * vael1 + vael3)
+    sessionStorage.setItem(datos, JSON.stringify(datos))
+
+    if (generoUsuario == "hombre") {
+
+        function pesoIdealh(alturaUsuario, vael0, vael1, vael3) {
+            return ((alturaUsuario - vael0) * vael1 + vael3)
+        }
+
+        const resultado1 = pesoIdealh(alturaUsuario, vael0, vael1, vael3)
+
+
+        let mensajeResultado = document.getElementById("resultado")
+
+        mensajeResultado.innerHTML = `Enhorabuena ${nombreUsuario} ! Tu peso ideal a conseguir seria de ${resultado1}. A continuacion te mostramos algun planes recomendados`
+
+    }
+
+    if (generoUsuario == "mujer") {
+
+        function pesoIdealm(alturaUsuario, vael0, vael2, vael3) {
+
+            return ((alturaUsuario - vael0) * vael2 + vael3)
+        }
+
+        const resultado2 = pesoIdealm(alturaUsuario, vael0, vael2, vael3)
+
+        let mensajeResultado = document.getElementById("resultado")
+
+        mensajeResultado.innerHTML = `Enhorabuena ${nombreUsuario} ! Tu peso ideal seria de ${resultado2} \n A continuacion te mostramos algun planes recomendados`
+
+        let planRecomendado = document.getElementById('plan1')
+        planRecomendado.innerHTML = '<h4>Para bajar de peso'
+
+    }
+
+    formulario.addEventListener('reset', () => {
+
+        let mensajeResultado = document.getElementById("resultado")
+        let planRecomendado = document.getElementById('plan1')
+        onclick = sessionStorage.clear(); mensajeResultado.innerHTML = ""; planRecomendado.innerHTML = ""
+    })
 }
 
-function pesoIdeal2(altura, vael0, vael2, vael3) {
-
-    return ((altura - vael0) * vael2 + vael3)
-}
-
-sessionStorage.setItem(datos,[nombre, altura, fdn, peso, genero])
