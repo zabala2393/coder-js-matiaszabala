98ea026df7fea8604de2fb94a54229f6b051af98
Matias Zabala <zabala@live.com.ar>
2025-02-11 15:17:34 -0300

    Correccion de carpeta unica

---
+++ src/components/CartList.jsx
@@ -0,0 +1,18 @@
+import { ListGroupItem, ListGroup, Button } from "react-bootstrap"
+import { useCart } from "../context/CartContext"
+import ItemCount from "./ItemCount"
+
+function CartList(item) {
+
+    const { cart } = useCart()
+    return (
+        <ListGroup >{cart.map(item =>
+        (<ListGroupItem key={item.id}>
+            {item.name}x{item.quantity}
+            ${item.price}
+            <ItemCount></ItemCount>
+        </ListGroupItem>))}
+        </ListGroup>
+    )
+}
+export default CartList
